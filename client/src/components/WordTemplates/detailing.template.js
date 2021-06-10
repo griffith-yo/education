41919ece2d4dbe9e9a99edcce9e8354cf0998d8e
griffith-yo <abaev.vladislav@gmail.com>
2021-06-10 17:49:36 +0300

    calibrating word fownload

--- client/src/components/WordTemplates/detailing.template.js
+++ client/src/components/WordTemplates/detailing.template.js
@@ -5,53 +5,171 @@ import {
   Header,
   ShadingType,
   UnderlineType,
+  AlignmentType,
+  convertInchesToTwip,
+  HeadingLevel,
+  LevelFormat,
+  Table,
+  TableCell,
+  TableRow,
+  VerticalAlign,
+  TextDirection,
 } from 'docx'
 
 export const detailingTemplate = (detailing) => {
   const resultString = detailing.passed ? 'Пройден' : 'Не пройден'
-  const resultColor = detailing.passed ? '#d1e7dd' : '#dc3545'
+  const resultBackgroundColor = detailing.passed ? '#d1e7dd' : '#f8d7da'
+  const resultTextColor = detailing.passed ? '#0f5132' : '#842029'
+
   return new Document({
+    creator: 'EntryPoint',
+    title: 'Detailing',
+    description: 'Some detailing info about user',
+    styles: {
+      default: {
+        heading1: {
+          run: {
+            size: 40,
+            bold: true,
+            color: resultTextColor,
+          },
+          paragraph: {
+            spacing: {
+              before: 120,
+              after: 120,
+              line: 320,
+            },
+            alignment: AlignmentType.CENTER,
+            shading: {
+              type: ShadingType.CLEAR,
+              fill: resultBackgroundColor,
+            },
+            border: {
+              bottom: {
+                color: 'auto',
+                space: 1,
+                value: 'single',
+                size: 6,
+              },
+            },
+          },
+        },
+        heading2: {
+          run: {
+            size: 26,
+            bold: true,
+            underline: {
+              type: UnderlineType.DOUBLE,
+              color: 'FF0000',
+            },
+          },
+          paragraph: {
+            spacing: {
+              before: 240,
+              after: 120,
+            },
+          },
+        },
+        listParagraph: {
+          run: {
+            color: '#FF0000',
+          },
+        },
+      },
+      paragraphStyles: [
+        {
+          id: 'aside',
+          name: 'Aside',
+          basedOn: 'Normal',
+          next: 'Normal',
+          run: {
+            size: 28,
+          },
+          paragraph: {
+            spacing: {
+              before: 100,
+              after: 100,
+              line: 300,
+            },
+            // indent: {
+            //   left: convertInchesToTwip(0.5),
+            // },
+            // spacing: {
+            //   line: 276,
+            // },
+          },
+        },
+        // {
+        //   id: 'wellSpaced',
+        //   name: 'Well Spaced',
+        //   basedOn: 'Normal',
+        //   quickFormat: true,
+        //   paragraph: {
+        //     spacing: {
+        //       line: 276,
+        //       before: 20 * 72 * 0.1,
+        //       after: 20 * 72 * 0.05,
+        //     },
+        //   },
+        // },
+        {
+          id: 'tableHead',
+          name: 'Table Head',
+          basedOn: 'Normal',
+          quickFormat: true,
+          run: {
+            size: 28,
+            bold: true,
+          },
+          paragraph: {
+            spacing: {
+              line: 276,
+              before: 20 * 72 * 0.1,
+              after: 20 * 72 * 0.05,
+            },
+          },
+        },
+        {
+          id: 'tableBody',
+          name: 'Table Body',
+          basedOn: 'Normal',
+          quickFormat: true,
+          run: {
+            size: 28,
+          },
+          paragraph: {
+            spacing: {
+              line: 276,
+              before: 20 * 72 * 0.1,
+              after: 20 * 72 * 0.05,
+            },
+          },
+        },
+      ],
+    },
+    numbering: {
+      config: [
+        {
+          reference: 'my-crazy-numbering',
+          levels: [
+            {
+              level: 0,
+              format: LevelFormat.LOWER_LETTER,
+              text: '%1)',
+              alignment: AlignmentType.LEFT,
+            },
+          ],
+        },
+      ],
+    },
     sections: [
       {
         properties: {},
-        headers: {
-          first: new Header({
-            // The first header
-            children: [
-              new Paragraph({
-                children: [
-                  new TextRun({
-                    text: detailing.user,
-                    bold: true,
-                    allCaps: true,
-                    break: 1,
-                  }),
-                ],
-              }),
-            ],
-          }),
-        },
-        // children: [
-        // new Paragraph({
-        //   children: [
-        //     new TextRun({
-        //       text: detailing.user,
-        //       bold: true,
-        //       allCaps: true,
-        //       break: 1,
-        //     }),
-        //   ],
-        //   underline: {
-        //     type: UnderlineType.DOUBLE,
-        //     color: '990011',
-        //   },
-        //   shading: {
-        //     type: ShadingType.SOLID,
-        //     color: resultColor,
-        //     // fill: resultColor,
-        //   },
-        // }),
         children: [
+          new Paragraph({
+            text: detailing.user,
+            heading: HeadingLevel.HEADING_1,
+          }),
           new Paragraph({
             children: [
               new TextRun({
@@ -61,6 +179,7 @@ export const detailingTemplate = (detailing) => {
                 text: detailing.group,
               }),
             ],
+            style: 'aside',
           }),
           new Paragraph({
             children: [
@@ -71,6 +190,7 @@ export const detailingTemplate = (detailing) => {
                 text: detailing.program,
               }),
             ],
+            style: 'aside',
           }),
           new Paragraph({
             children: [
@@ -81,6 +201,7 @@ export const detailingTemplate = (detailing) => {
                 text: detailing.attempt,
               }),
             ],
+            style: 'aside',
           }),
           new Paragraph({
             children: [
@@ -91,6 +212,7 @@ export const detailingTemplate = (detailing) => {
                 text: detailing.scores + '/' + detailing.scoresMax,
               }),
             ],
+            style: 'aside',
           }),
           new Paragraph({
             children: [
@@ -100,6 +222,91 @@ export const detailingTemplate = (detailing) => {
               new TextRun({
                 text: resultString,
               }),
+            ],
+            style: 'aside',
+          }),
+          new Table({
+            rows: [
+              new TableRow({
+                children: [
+                  new TableCell({
+                    children: [
+                      new Paragraph({ text: 'Номер', style: 'tableHead' }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                  }),
+                  new TableCell({
+                    children: [
+                      new Paragraph({ text: 'Вопрос', style: 'tableHead' }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                  }),
+                  new TableCell({
+                    children: [
+                      new Paragraph({ text: 'Ответ', style: 'tableHead' }),
+                      // new Paragraph({}),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // textDirection: TextDirection.BOTTOM_TO_TOP_LEFT_TO_RIGHT,
+                  }),
+                  new TableCell({
+                    children: [
+                      new Paragraph({ text: 'Результат', style: 'tableHead' }),
+                    ],
+                    verticalAlign: VerticalAlign.CENTER,
+                    // textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                  }),
+                ],
+              }),
+              ...detailing.questions.map(
+                (question, index) =>
+                  new TableRow({
+                    children: [
+                      new TableCell({
+                        children: [
+                          new Paragraph({
+                            text: (index + 1).toString(),
+                            style: 'tableBody',
+                          }),
+                        ],
+                        verticalAlign: VerticalAlign.CENTER,
+                      }),
+                      new TableCell({
+                        children: [
+                          new Paragraph({
+                            text: question.questionName,
+                            style: 'tableBody',
+                          }),
+                        ],
+                        verticalAlign: VerticalAlign.CENTER,
+                      }),
+                      new TableCell({
+                        children: [
+                          ...question.questionAnswers
+                            .map((answer, index) =>
+                              answer.correctness
+                                ? new Paragraph({
+                                    text: answer.answerName,
+                                    style: 'tableBody',
+                                  })
+                                : ''
+                            )
+                            .filter(Boolean),
+                        ],
+                        verticalAlign: VerticalAlign.CENTER,
+                      }),
+                      new TableCell({
+                        children: [
+                          new Paragraph({
+                            text: question.success ? 'Верно' : 'Неверно',
+                            style: 'tableBody',
+                          }),
+                        ],
+                        verticalAlign: VerticalAlign.CENTER,
+                      }),
+                    ],
+                  })
+              ),
             ],
           }),
         ],
